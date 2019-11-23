23037108eb17733330437bb277cb9d4bdab45647
Andrey <kouznts@ya.ru>
2019-11-23 14:30:35 +0400

    добавить шаблон

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace internet_technology_2019_coursework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
