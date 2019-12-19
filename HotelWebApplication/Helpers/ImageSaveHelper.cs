de8aef04d0643ee65cf2cd1b9adb10567b2b57a2
Andrey <kouznts@ya.ru>
2019-12-19 11:33:23 +0400

    описать SaveImage(HttpPostedFileBase imageData)

---
+++ HotelWebApplication/Helpers/ImageSaveHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+
+namespace HotelWebApplication.Helpers
+{
+    public class ImageSaveHelper
+    {
+        public static string SaveImage(HttpPostedFileBase imageData)
+        {
+            var ext = Path.GetExtension(imageData.FileName);
+            var guid = Guid.NewGuid();
+            var result = guid.ToString() + ext;
+
+            if (imageData == null)
+                return null;
+
+            var filePathOriginal = HostingEnvironment.MapPath("/Content/Images/Uploads/");
+            string savedFileName = Path.Combine(filePathOriginal ?? throw new InvalidOperationException(), result);
+
+            imageData.SaveAs(savedFileName);
+
+            return $"/Content/Images/Uploads/{result}";
+        }
+    }
+}
