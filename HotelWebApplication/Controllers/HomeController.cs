120cd59342ff9f28f9c60e226fc2ed16d4a6b33f
Andrey <kouznts@ya.ru>
2019-12-19 10:54:34 +0400

    пустой контроллер Home

--- HotelWebApplication/Controllers/HomeController.cs
+++ HotelWebApplication/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Web.Mvc;
 using HotelWebApplication.Dal;
 using HotelWebApplication.Models;
@@ -9,30 +10,6 @@ namespace HotelWebApplication.Controllers
     public class HomeController : Controller
     {
         private HotelContext db = new HotelContext();
-
-        public ActionResult Index()
-        {
-            IEnumerable<Room> rooms = db.Rooms;
-            ViewBag.Rooms = rooms;
-
-            return View();
-        }
-
-        [HttpGet] public ActionResult Book(int id)
-        {
-            ViewBag.BookingId = id;
-
-            return View();
-        }
-
-        [HttpPost] public string Book(Booking booking)
-        {
-            booking.BookingDateTime = DateTime.Now;
-
-            db.Bookings.Add(booking);
-            db.SaveChanges();
-
-            return "номер забронирован";
-        }
+   
     }
 }
