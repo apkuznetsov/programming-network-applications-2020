be7ceebb1e79318c1301034eda5d712416ab60f4
Andrey <kouznts@ya.ru>
2019-12-20 16:35:56 +0400

    описать отображаемые имена атрибутов Бронирования

--- HotelWebApplication/Models/Booking.cs
+++ HotelWebApplication/Models/Booking.cs
@@ -7,18 +7,24 @@ namespace HotelWebApplication.Models
     {
         [Key] public int Id { get; set; }
 
+        [Display(Name = "Серия и номер паспорта клиента")]
         public int ClientPassportSeriesAndNumber { get; set; }
 
+        [Display(Name = "ФИО клиента")]
         public string ClientName { get; set; }
 
+        [Display(Name = "Номер комнаты")]
         public int RoomId { get; set; }
 
         public DateTime BookingDateTime { get; set; }
 
+        [Display(Name = "Дата заезда")]
         public DateTime ArrivalDateTime { get; set; }
 
+        [Display(Name = "Дата отъезда")]
         public DateTime DepartureDateTime { get; set; }
 
+        [Display(Name = "Стоимость заказа")]
         public int OrderPrice { get; set; }
     }
 }
