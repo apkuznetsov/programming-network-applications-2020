1786fde9b09408e6fc3829dedc6af6589ce692e4
Andrey <kouznts@ya.ru>
2019-12-21 19:59:29 +0400

    описать All() в BookingController

--- HotelWebApplication/Controllers/BookingController.cs
+++ HotelWebApplication/Controllers/BookingController.cs
@@ -1,4 +1,6 @@
 ﻿using HotelWebApplication.Dal;
+using HotelWebApplication.Models;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace HotelWebApplication.Controllers
@@ -6,5 +8,18 @@ namespace HotelWebApplication.Controllers
     public class BookingController : Controller
     {
         private HotelContext db = new HotelContext();
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("All", "Hotel");
+        }
+
+        public ActionResult All()
+        {
+            IEnumerable<Booking> bookings = db.Bookings;
+            ViewBag.Bookings = bookings;
+
+            return View();
+        }
     }
 }
