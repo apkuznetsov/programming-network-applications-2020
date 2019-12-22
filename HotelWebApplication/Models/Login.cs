7fc23b0ba3f2690c1af6e4a21874ab9ee096ee3e
Andrey <kouznts@ya.ru>
2019-12-22 09:26:29 +0400

    описать модель Login

---
+++ HotelWebApplication/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelWebApplication.Models
+{
+    public class Login
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
