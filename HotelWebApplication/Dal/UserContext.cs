d0e793e49af05040c5c550a48380c69f2aa8eca5
Andrey <kouznts@ya.ru>
2019-12-22 09:23:48 +0400

    описать контекст данных UserContext

---
+++ HotelWebApplication/Dal/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using HotelWebApplication.Models;
+
+namespace HotelWebApplication.Dal
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() :
+            base("DefaultConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
