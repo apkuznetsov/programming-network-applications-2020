e7cdf8a9fbcb67026fdbf45b89ba4f6631d23f16
Andrey <kouznts@ya.ru>
2019-12-22 08:50:49 +0400

    описать ClientController

---
+++ HotelWebApplication/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using HotelWebApplication.Dal;
+using HotelWebApplication.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace HotelWebApplication.Controllers
+{
+    public class ClientController : Controller
+    {
+        private HotelContext db = new HotelContext();
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("All", "Client");
+        }
+
+        public ActionResult All()
+        {
+            IEnumerable<Client> clients = db.Clients;
+            ViewBag.Clients = clients;
+
+            return View();
+        }
+    }
+}
