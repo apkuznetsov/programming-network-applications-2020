f88c293dec679ca271e363da36289c87c5538fe2
Andrey <kouznts@ya.ru>
2020-03-18 19:40:01 +0400

    class PagedData<T>

---
+++ Helpers/PagedData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HotelWebApp.Helpers
+{
+    public class PagedData<T> where T : class
+    {
+        public IEnumerable<T> Data { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
